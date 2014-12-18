aa7a55ef1217ac1a9a5bc72883c0fa23f648e90e
Matt Cooper <matt@matthewcooper.net>
2014-12-19 10:29:24 +1300

    Adding correct validation code

--- src/CronValidator.php
+++ src/CronValidator.php
@@ -29,6 +29,46 @@ class CronValidator extends Validator
     }
 
     /**
+     * Build Cron Regex
+     *
+     * Taken from http://stackoverflow.com/questions/235504/validating-crontab-entries-w-php
+     *
+     * @author Jordi Salvat i Alabart - with thanks to <a href="www.salir.com">Salir.com</a>.
+     */
+    function buildRegexp()
+    {
+        $numbers = array(
+            'min' => '[0-5]?\d',
+            'hour' => '[01]?\d|2[0-3]',
+            'day' => '0?[1-9]|[12]\d|3[01]',
+            'month' => '[1-9]|1[012]',
+            'dow' => '[0-6]'
+        );
+
+        foreach ($numbers as $field => $number) {
+            $range = "(?:$number)(?:-(?:$number)(?:\/\d+)?)?";
+            $field_re[$field] = "\*(?:\/\d+)?|$range(?:,$range)*";
+        }
+
+        $field_re['month'].='|jan|feb|mar|apr|may|jun|jul|aug|sep|oct|nov|dec';
+        $field_re['dow'].='|mon|tue|wed|thu|fri|sat|sun';
+
+        $fields_re = '(' . join(')\s+(', $field_re) . ')';
+
+        $replacements = '@reboot|@yearly|@annually|@monthly|@weekly|@daily|@midnight|@hourly';
+
+        return '^\s*(' .
+                '$' .
+                '|#' .
+                '|\w+\s*=' .
+                "|$fields_re\s+" .
+                "|($replacements)\s+" .
+                ')' .
+                '([^\\s]+)\\s+' .
+                '(.*)$';
+    }
+
+    /**
      * Allow only valid cron expressions
      *
      * @param string $attribute
@@ -37,6 +77,6 @@ class CronValidator extends Validator
      */
     protected function validateCronExpression($attribute, $value)
     {
-        return (bool) preg_match("/^[A-Za-z\s-_]+$/", $value);
+        return (bool) preg_match("/" . $this->buildRegexp() . "/", $value);
     }
 }
